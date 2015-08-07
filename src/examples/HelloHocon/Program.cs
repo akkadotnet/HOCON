e4441c8d7f6235e7ca60a4d6a6547e6030ee7f8e
John Haigh <haighis@gmail.com>
2015-08-06 23:34:45 -0400

    - Create initial project structure - up for discussion - Bring over Hocon classes from akka.net project. No refactoring, straight   copy with some namespace changes. - structure is up for discussion   - Add Newtonsoft nuget package dependency   - Add Hocon.Tests Project.   - Create Examples solution folder   - Create HelloHocon example project with one sample in Program.cs   - Add RELEASE_NOTES.md

---
+++ src/examples/HelloHocon/Program.cs
@@ -0,0 +1,28 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Hocon Project">
+//     Copyright (C) 2009-2015 Typesafe Inc. <http://www.typesafe.com>
+//     Copyright (C) 2013-2015 Akka.NET project <https://github.com/akkadotnet/hocon>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Configuration;
+using System;
+
+namespace HelloHocon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hocon = @"
+root {
+  simple-string = ""Hello Hocon""  
+}
+";
+            var config = ConfigurationFactory.ParseString(hocon);
+            var val = config.GetString("root.simple-string");
+            Console.WriteLine("Hocon says: " + val);
+            Console.ReadKey();
+        }
+    }
+}
