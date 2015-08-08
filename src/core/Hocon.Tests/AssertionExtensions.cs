f036e55cd2116456ccd54992778880a6f08afe9f
rogeralsing <rogeralsing@gmail.com>
2015-08-08 12:39:37 +0200

    Added HOCON tests using NUnit

---
+++ src/core/Hocon.Tests/AssertionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hocon.Tests
+{
+    public static class AssertionExtensions
+    {
+        public static void ShouldBe<T>(this T self, T other)
+        {
+            Assert.AreEqual(other, self);
+        }
+    }
+}
