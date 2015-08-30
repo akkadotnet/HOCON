489837a792e33f184241e7e66e185af65376d3ab
Adam Hathcock <adam@hathcock.co.uk>
2015-08-30 22:41:34 +0100

    Add test for MF.Hocon

---
+++ src/examples/HelloMFHocon/Program.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Hocon Project">
+//     Copyright (C) 2009-2015 Typesafe Inc. <http://www.typesafe.com>
+//     Copyright (C) 2013-2015 Akka.NET project <https://github.com/akkadotnet/hocon>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using Akka.Configuration;
+using System;
+using Akka.Hocon.Configuration;
+using Microsoft.Framework.Configuration;
+using Microsoft.Framework.Runtime;
+
+namespace HelloMFHocon
+{
+    public class Program
+    {
+        private readonly IApplicationEnvironment applicationEnvironment;
+
+        public Program(IApplicationEnvironment applicationEnvironment)
+        {
+            this.applicationEnvironment = applicationEnvironment;
+        }
+
+        public void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder(applicationEnvironment.ApplicationBasePath);
+            builder.AddHoconFile("hello.hocon");
+            var config = builder.Build();
+            var val = config.Get("root:simple-string");
+            Console.WriteLine("Hocon says: " + val);
+            Console.ReadLine();
+        }
+    }
+}
