42fde3c83a07c350de964617a1a4c86ab8e78e8d
Jared Lobberecht <jared@lobberecht.com>
2015-11-24 19:14:20 -0600

    Added Hocon List Configuration Element Collection Examples

---
+++ src/examples/ConfigurationFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using Akka.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var section = ConfigurationManager.GetSection("testsection") as TestConfigurationSection;
+            var config = ConfigurationFactory.FromConfigurationElementCollection(section.HoconList);
+
+            string resulta = config.GetString("root.simple-string-a");
+            string resultb = config.GetString("root.simple-string-b");
+            string resultc = config.GetString("root.simple-string-c");
+
+            Console.WriteLine("root.simple-string-a: " + resulta);
+            Console.WriteLine("root.simple-string-b: " + resultb);
+            Console.WriteLine("root.simple-string-c: " + resultc);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/examples/WebConfigTransforms/Global.asax.cs
@@ -0,0 +1,37 @@
+﻿using Akka.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebConfigTransforms
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static Config HoconConfig;
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RegisterRoutes(RouteTable.Routes);
+
+            var section = ConfigurationManager.GetSection("testsection") as TestConfigurationSection;
+            HoconConfig = ConfigurationFactory.FromConfigurationElementCollection(section.HoconList);
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
