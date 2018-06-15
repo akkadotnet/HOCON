c43c81d6d16484abbcc47eb819a57037eb081561
Gregorius Soedharmo <arkatufus@yahoo.com>
2018-06-16 04:44:03 +0700

    Add unquoted string whitespace handling spec

--- src/HOCON.Tests/UnquotedString.cs
+++ src/HOCON.Tests/UnquotedString.cs
@@ -9,22 +9,45 @@ namespace Hocon.Tests
 {
     public class UnquotedString
     {
+        /*
+         * From: String value concatenation.
+         */
+
+        /*
+         * FACT:
+         * Whitespace before the first and after the last simple value must be discarded
+         */
         [Fact]
         public void ShouldRemoveAllLeadingWhitespace()
         {
-            var hocon = $"a = {Whitespace.Whitespaces}literal";
+            var hocon = $"a = {Whitespace.Whitespaces}literal value";
             var config = ConfigurationFactory.ParseString(hocon);
 
-            Assert.Equal("literal", config.GetString("a"));
+            Assert.Equal("literal value", config.GetString("a"));
         }
 
         [Fact]
         public void ShouldRemoveAllTrailingWhitespace()
         {
-            var hocon = $"a = literal{Whitespace.Whitespaces}";
+            var hocon = $"a = literal value{Whitespace.Whitespaces}";
             var config = ConfigurationFactory.ParseString(hocon);
 
-            Assert.Equal("literal", config.GetString("a"));
+            Assert.Equal("literal value", config.GetString("a"));
+        }
+
+        /*
+         * FACT:
+         * As long as simple values are separated only by non-newline whitespace, 
+         * the whitespace between them is preserved and the values, along with the whitespace, 
+         * are concatenated into a string.
+         */
+        [Fact]
+        public void ShouldPreserveWhitespacesInTheMiddle()
+        {
+            var hocon = $"a = literal{Whitespace.Whitespaces}value";
+            var config = ConfigurationFactory.ParseString(hocon);
+
+            Assert.Equal($"literal{Whitespace.Whitespaces}value", config.GetString("a"));
         }
     }
 }
