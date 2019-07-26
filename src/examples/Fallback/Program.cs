4eb901cab5d089365c662cbef03e476acaf1b02d
Gregorius Soedharmo <arkatufus@yahoo.com>
2019-07-26 23:36:18 +0700

    Substitution environment variable fallback sample (#100)
    * Add environment variable fallback sample to the Fallback sample project
    
    * Remove launchSettings.json ignore rule from .gitignore
    
    * Add missing properties folder
    
    * Make sample more robust for missing environment value

--- src/examples/Fallback/Program.cs
+++ src/examples/Fallback/Program.cs
@@ -14,10 +14,13 @@ namespace Fallback
     {
         static void Main(string[] args)
         {
+            // The "environment-property" property is set through environment variable.
+            // On debug mode, it is set inside the debug tab in the project properties.
             var baseHocon = @"
 root {
    some-property1 = 123
-   some-property2 = 234  
+   some-property2 = 234 
+   environment-property = ${?ENV_MY_PROPERTY} 
 }
 ";
             var userHocon = @"
@@ -38,6 +41,12 @@ root {
             Console.WriteLine("root.some-property1 = {0}", val1);
             Console.WriteLine("root.some-property2 = {0}", val2);
             Console.WriteLine("root.some-property3 = {0}", val3);
+
+            if (merged.HasPath("root.environment-property"))
+            {
+                var envVal = merged.GetString("root.environment-property");
+                Console.WriteLine("root.environment-property = {0}", envVal);
+            }
 
             Console.ReadKey();
         }
