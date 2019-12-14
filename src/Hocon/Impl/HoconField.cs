5f5cd67b6df6504c7b037d6bbf6511a17d725abc
IgorFedchenko <igor@fedchenko.pro>
2019-12-14 16:27:06 +0300

    Fixed related test

--- src/Hocon/Impl/HoconField.cs
+++ src/Hocon/Impl/HoconField.cs
@@ -1,4 +1,4 @@
-﻿//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // <copyright file="HoconField.cs" company="Hocon Project">
 //     Copyright (C) 2009-2018 Lightbend Inc. <http://www.lightbend.com>
 //     Copyright (C) 2013-2018 .NET Foundation <https://github.com/akkadotnet/hocon>
@@ -96,7 +96,7 @@ namespace Hocon
             if (value == null)
                 return;
 
-            if (value.Type == HoconType.Literal)
+            if (value.Type != HoconType.Object)
             {
                 foreach (var item in _internalValues)
                 {
