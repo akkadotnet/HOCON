c3dad9b0a259ad98e1ec39ea00da984c6329244b
Aaron Stannard <aaron@petabridge.com>
2020-01-13 16:42:29 -0600

    added IsString extension method
    Needed for CachingConfig inside https://github.com/akkadotnet/akka.net/pull/4128

--- src/Hocon/Extensions/HoconElementExtensions.cs
+++ src/Hocon/Extensions/HoconElementExtensions.cs
@@ -15,5 +15,37 @@ namespace Hocon.Extensions
         {
             return hoconElement is HoconSubstitution ? hoconElement : hoconElement.Clone(newParent);
         }
+
+        /// <summary>
+        /// Checks to see if this <see cref="IHoconElement"/> is a literal and if it is,
+        /// if it's a supported string type.
+        /// </summary>
+        /// <param name="literal">The HOCON element to check.</param>
+        public static bool IsString(this IHoconElement literal)
+        {
+            if (!(literal is HoconLiteral l)) return false;
+            switch (l.LiteralType)
+            {
+                case HoconLiteralType.QuotedString:
+                case HoconLiteralType.UnquotedString:
+                case HoconLiteralType.TripleQuotedString:
+                case HoconLiteralType.Whitespace:
+                    return true;
+                case HoconLiteralType.Null:
+                    break;
+                case HoconLiteralType.Bool:
+                    break;
+                case HoconLiteralType.Long:
+                    break;
+                case HoconLiteralType.Hex:
+                    break;
+                case HoconLiteralType.Octal:
+                    break;
+                case HoconLiteralType.Double:
+                    break;
+            }
+
+            return false;
+        }
     }
 }
