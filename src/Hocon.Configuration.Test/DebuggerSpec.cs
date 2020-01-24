0d9e2d35b1e9719e75d5fe2a1ef1509b36a7c118
Aaron Stannard <aaron@petabridge.com>
2020-01-23 20:48:42 -0600

    HOCON Debugger tools

---
+++ src/Hocon.Configuration.Test/DebuggerSpec.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Hocon.Debugger;
+using Xunit;
+
+namespace Hocon.Configuration.Tests
+{
+    public class DebuggerSpec
+    {
+        [Fact]
+        public void Should_dump_stand_alone_HOCON()
+        {
+            var myHocon = ConfigurationFactory.ParseString(@"akka{
+                actor.provider = cluster
+                deployment{
+                    /foo {
+                        dispatcher = bar
+                    }
+                }
+            }");
+
+            var dump = myHocon.DumpConfig();
+            dump.Should().Contain(myHocon.PrettyPrint(2));
+        }
+
+        [Fact]
+        public void Should_dump_HOCON_with_1_Fallback()
+        {
+            var myHocon1 = ConfigurationFactory.ParseString(@"akka{
+                actor.provider = cluster
+                deployment{
+                    /foo {
+                        dispatcher = bar
+                    }
+                }
+            }");
+
+            var myHocon2 = ConfigurationFactory.ParseString(@"akka{
+                debug{
+                    received  = on
+                }
+                actor.provider = cluster
+                deployment{
+                    /foo {
+                        dispatcher = foo
+                    }
+                    /bar {
+                        mailbox = ""myFoo""
+                    }
+                }
+            }");
+
+            var fullHocon = myHocon1.WithFallback(myHocon2);
+
+            var dump = fullHocon.DumpConfig();
+            dump.Should().Contain(myHocon1.PrettyPrint(2));
+            dump.Should().Contain(myHocon2.PrettyPrint(2));
+        }
+
+        [Fact]
+        public void Should_dump_HOCON_with_2_Fallback()
+        {
+            var myHocon1 = ConfigurationFactory.ParseString(@"akka{
+                actor.provider = cluster
+                deployment{
+                    /foo {
+                        dispatcher = bar
+                    }
+                }
+            }");
+
+            var myHocon2 = ConfigurationFactory.ParseString(@"akka{
+                debug{
+                    received  = on
+                }
+                actor.provider = cluster
+                deployment{
+                    /foo {
+                        dispatcher = foo
+                    }
+                    /bar {
+                        mailbox = ""myFoo""
+                    }
+                }
+            }");
+
+            var myHocon3 = ConfigurationFactory.ParseString(@"akka{
+                remote{
+                    transport = ""foo""
+                }
+            }");
+
+            var fullHocon = myHocon1
+                .SafeWithFallback(myHocon2)
+                .SafeWithFallback(myHocon3);
+
+            fullHocon.GetString("akka.remote.transport").Should().Be("foo");
+
+            var dump = fullHocon.DumpConfig();
+            dump.Should().Contain(myHocon1.PrettyPrint(2));
+            dump.Should().Contain(myHocon2.PrettyPrint(2));
+            dump.Should().Contain(myHocon3.PrettyPrint(2));
+        }
+    }
+}
---
+++ src/Hocon.Configuration/Debugger/DebuggingExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hocon.Debugger
+{
+    /// <summary>
+    /// Debugging extensions for <see cref="Config"/> objects.
+    /// </summary>
+    public static class DebuggingExtensions
+    {
+        /// <summary>
+        /// Dumps all of the fallbacks in the order in which they would be resolved.
+        /// </summary>
+        /// <param name="c">The top-level config.</param>
+        /// <returns>A stringified list of fallbacks</returns>
+        public static string DumpConfig(this Config c)
+        {
+            var sb = new StringBuilder();
+            var hoconCount = 0;
+
+            void AppendHocon(Config config, int i)
+            {
+                sb.AppendFormat("HOCON{0}", i)
+                    .AppendLine()
+                    .Append(config.PrettyPrint(2))
+                    .AppendLine();
+            }
+
+            AppendHocon(c, hoconCount);
+
+            var current = c;
+            while (current.Fallback != null)
+            {
+                // add a header here
+                sb.AppendLine().AppendLine("------------");
+                current = current.Fallback;
+                AppendHocon(current, ++hoconCount);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
