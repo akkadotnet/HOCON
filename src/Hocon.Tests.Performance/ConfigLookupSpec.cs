3adc38dd115413fd53f9d3fe5d2b81ddd2d851b5
Aaron Stannard <aaron@petabridge.com>
2020-02-24 16:24:52 -0600

    added memory measurements and a 1 deep benchmark

--- src/Hocon.Tests.Performance/ConfigLookupSpec.cs
+++ src/Hocon.Tests.Performance/ConfigLookupSpec.cs
@@ -10,6 +10,10 @@ namespace Hocon.Tests.Performance
 
         private Counter _throughputCounter;
 
+        private static readonly Config Config_1_Deep = "a = 10";
+        private static readonly string StringPath_1_Deep = "a";
+        private static readonly HoconPath HoconPathPath_1_Deep = new HoconPath(new string[] { "a"});
+
         private static readonly Config Config_10_Deep = "a.b.c.d.e.f.g.h.i.j = 10";
         private static readonly string StringPath_10_Deep = "a.b.c.d.e.f.g.h.i.j";
         private static readonly HoconPath HoconPathPath_10_Deep = new HoconPath(new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j" });
@@ -30,6 +34,7 @@ namespace Hocon.Tests.Performance
              TestMode = TestMode.Measurement)]
         [CounterThroughputAssertion(ConfigLookupName, MustBe.GreaterThan, MinimumAcceptableOperationsPerSecond)]
         [GcMeasurement(GcMetric.TotalCollections, GcGeneration.AllGc)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
         public void Config_Lookup_String_10_Deep_throughput(BenchmarkContext context)
         {
             Config_10_Deep.GetString(StringPath_10_Deep);
@@ -42,6 +47,7 @@ namespace Hocon.Tests.Performance
              TestMode = TestMode.Measurement)]
         [CounterThroughputAssertion(ConfigLookupName, MustBe.GreaterThan, MinimumAcceptableOperationsPerSecond)]
         [GcMeasurement(GcMetric.TotalCollections, GcGeneration.AllGc)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
         public void Config_Lookup_HoconPath_10_Deep_throughput(BenchmarkContext context)
         {
             Config_10_Deep.GetString(HoconPathPath_10_Deep);
@@ -54,6 +60,7 @@ namespace Hocon.Tests.Performance
              TestMode = TestMode.Measurement)]
         [CounterThroughputAssertion(ConfigLookupName, MustBe.GreaterThan, MinimumAcceptableOperationsPerSecond)]
         [GcMeasurement(GcMetric.TotalCollections, GcGeneration.AllGc)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
         public void Config_Lookup_String_4_Deep_throughput(BenchmarkContext context)
         {
             Config_4_Deep.GetString(StringPath_4_Deep);
@@ -66,9 +73,35 @@ namespace Hocon.Tests.Performance
              TestMode = TestMode.Measurement)]
         [CounterThroughputAssertion(ConfigLookupName, MustBe.GreaterThan, MinimumAcceptableOperationsPerSecond)]
         [GcMeasurement(GcMetric.TotalCollections, GcGeneration.AllGc)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
         public void Config_Lookup_HoconPath_4_Deep_throughput(BenchmarkContext context)
         {
             Config_4_Deep.GetString(HoconPathPath_4_Deep);
+            _throughputCounter.Increment();
+        }
+
+        [PerfBenchmark(
+            Description = "Tests how quickly Config can perform a 1 deep path lookup using a string path",
+            RunMode = RunMode.Throughput, NumberOfIterations = 13, RunTimeMilliseconds = 1000,
+            TestMode = TestMode.Measurement)]
+        [CounterThroughputAssertion(ConfigLookupName, MustBe.GreaterThan, MinimumAcceptableOperationsPerSecond)]
+        [GcMeasurement(GcMetric.TotalCollections, GcGeneration.AllGc)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+        public void Config_Lookup_String_1_Deep_throughput(BenchmarkContext context)
+        {
+            Config_1_Deep.GetString(StringPath_1_Deep);
+            _throughputCounter.Increment();
+        }
+
+        [PerfBenchmark(
+            Description = "Tests how quickly Config can perform a 1 deep path lookup using a HoconPath path",
+            RunMode = RunMode.Throughput, NumberOfIterations = 13, RunTimeMilliseconds = 1000,
+            TestMode = TestMode.Measurement)]
+        [CounterThroughputAssertion(ConfigLookupName, MustBe.GreaterThan, MinimumAcceptableOperationsPerSecond)]
+        [GcMeasurement(GcMetric.TotalCollections, GcGeneration.AllGc)]
+        public void Config_Lookup_HoconPath_1_Deep_throughput(BenchmarkContext context)
+        {
+            Config_1_Deep.GetString(HoconPathPath_1_Deep);
             _throughputCounter.Increment();
         }
     }
