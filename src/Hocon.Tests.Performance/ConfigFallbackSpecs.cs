5321cf483d27bea934e196ebb271ae60f153f836
Aaron Stannard <aaron@petabridge.com>
2020-03-05 14:32:18 -0600

    adding benchmarks for measuring fallback create and lookup performance

---
+++ src/Hocon.Tests.Performance/ConfigFallbackSpecs.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NBench;
+
+namespace Hocon.Tests.Performance
+{
+    public class ConfigFallbackSpecs
+    {
+        private const string CreateFallbackCounterName = "CreateFallbackOp";
+        private const string LookupFallbackCounterName = "LookupFallbackOp";
+
+        private Counter _createFallbackCounter;
+        private Counter _lookupFallbackOp;
+
+        public static readonly Config C1 = @"
+            akka{
+                provider = cluster
+                remote.dot-netty.tcp.port = 8110
+            }
+        ";
+
+        public static readonly Config C2 = @"
+            akka{
+                loglevel = DEBUG
+                remote.dot-netty.tcp.hostname = 0.0.0.0
+            }
+        ";
+
+        public static readonly Config C3 = @"
+            akka{
+                remote.dot-netty.tcp.public-hostname = localhost
+            }
+        ";
+
+        private static readonly Config L1 = C1.WithFallback(C2);
+
+        private static readonly Config L2 = C1.WithFallback(C3);
+
+        [PerfSetup]
+        public void Setup(BenchmarkContext context)
+        {
+            _createFallbackCounter = context.GetCounter(CreateFallbackCounterName);
+            _lookupFallbackOp = context.GetCounter(LookupFallbackCounterName);
+        }
+
+        [PerfBenchmark(
+            Description = "Tests how quickly Config can add a single fallback",
+            RunMode = RunMode.Throughput, NumberOfIterations = 13, RunTimeMilliseconds = 1000,
+            TestMode = TestMode.Measurement)]
+        [CounterMeasurement(CreateFallbackCounterName)]
+        [GcMeasurement(GcMetric.TotalCollections, GcGeneration.AllGc)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+        public void Create_Config_Fallback_1_Deep(BenchmarkContext context)
+        {
+            var newConfig = C1.WithFallback(C2);
+            _createFallbackCounter.Increment();
+        }
+
+        [PerfBenchmark(
+            Description = "Tests how quickly Config can add a two fallbacks",
+            RunMode = RunMode.Throughput, NumberOfIterations = 13, RunTimeMilliseconds = 1000,
+            TestMode = TestMode.Measurement)]
+        [CounterMeasurement(CreateFallbackCounterName)]
+        [GcMeasurement(GcMetric.TotalCollections, GcGeneration.AllGc)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+        public void Create_Config_Fallback_2_Deep(BenchmarkContext context)
+        {
+            var newConfig = C1.WithFallback(C2).WithFallback(C3);
+            _createFallbackCounter.Increment();
+        }
+
+        [PerfBenchmark(
+            Description = "Tests how quickly Config can be looked up from the second fallback",
+            RunMode = RunMode.Throughput, NumberOfIterations = 13, RunTimeMilliseconds = 1000,
+            TestMode = TestMode.Measurement)]
+        [CounterMeasurement(CreateFallbackCounterName)]
+        [GcMeasurement(GcMetric.TotalCollections, GcGeneration.AllGc)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+        public void Lookup_Config_Fallback_2_Deep(BenchmarkContext context)
+        {
+            L1.GetString("akka.loglevel");
+            _createFallbackCounter.Increment();
+        }
+
+        [PerfBenchmark(
+            Description = "Tests how quickly Config can be looked up from the third  fallback",
+            RunMode = RunMode.Throughput, NumberOfIterations = 13, RunTimeMilliseconds = 1000,
+            TestMode = TestMode.Measurement)]
+        [CounterMeasurement(CreateFallbackCounterName)]
+        [GcMeasurement(GcMetric.TotalCollections, GcGeneration.AllGc)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+        public void Lookup_Config_Fallback_3_Deep(BenchmarkContext context)
+        {
+            L2.GetString("akka.remote.dot-netty.tcp.public-hostname");
+            _createFallbackCounter.Increment();
+        }
+    }
+}
