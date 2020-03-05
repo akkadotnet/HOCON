3351846d31be8f98370b9f79308aeecf9fb48e9a
Aaron Stannard <aaron@petabridge.com>
2020-03-05 14:37:48 -0600

    split config specs

--- src/Hocon.Tests.Performance/ConfigFallbackSpecs.cs
+++ src/Hocon.Tests.Performance/ConfigFallbackSpecs.cs
@@ -8,10 +8,8 @@ namespace Hocon.Tests.Performance
     public class ConfigFallbackSpecs
     {
         private const string CreateFallbackCounterName = "CreateFallbackOp";
-        private const string LookupFallbackCounterName = "LookupFallbackOp";
 
         private Counter _createFallbackCounter;
-        private Counter _lookupFallbackOp;
 
         public static readonly Config C1 = @"
             akka{
@@ -41,7 +39,6 @@ namespace Hocon.Tests.Performance
         public void Setup(BenchmarkContext context)
         {
             _createFallbackCounter = context.GetCounter(CreateFallbackCounterName);
-            _lookupFallbackOp = context.GetCounter(LookupFallbackCounterName);
         }
 
         [PerfBenchmark(
@@ -69,31 +66,68 @@ namespace Hocon.Tests.Performance
             var newConfig = C1.WithFallback(C2).WithFallback(C3);
             _createFallbackCounter.Increment();
         }
+    }
+
+    public class ConfigFallbackLookupSpecs
+    {
+        private const string LookupFallbackCounterName = "LookupFallbackOp";
+
+        private Counter _lookupFallbackOp;
+
+        public static readonly Config C1 = @"
+            akka{
+                provider = cluster
+                remote.dot-netty.tcp.port = 8110
+            }
+        ";
+
+        public static readonly Config C2 = @"
+            akka{
+                loglevel = DEBUG
+                remote.dot-netty.tcp.hostname = 0.0.0.0
+            }
+        ";
+
+        public static readonly Config C3 = @"
+            akka{
+                remote.dot-netty.tcp.public-hostname = localhost
+            }
+        ";
+
+        private static readonly Config L1 = C1.WithFallback(C2);
+
+        private static readonly Config L2 = C1.WithFallback(C3);
+
+        [PerfSetup]
+        public void Setup(BenchmarkContext context)
+        {
+            _lookupFallbackOp = context.GetCounter(LookupFallbackCounterName);
+        }
 
         [PerfBenchmark(
             Description = "Tests how quickly Config can be looked up from the second fallback",
             RunMode = RunMode.Throughput, NumberOfIterations = 13, RunTimeMilliseconds = 1000,
             TestMode = TestMode.Measurement)]
-        [CounterMeasurement(CreateFallbackCounterName)]
+        [CounterMeasurement(LookupFallbackCounterName)]
         [GcMeasurement(GcMetric.TotalCollections, GcGeneration.AllGc)]
         [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
         public void Lookup_Config_Fallback_2_Deep(BenchmarkContext context)
         {
             L1.GetString("akka.loglevel");
-            _createFallbackCounter.Increment();
+            _lookupFallbackOp.Increment();
         }
 
         [PerfBenchmark(
             Description = "Tests how quickly Config can be looked up from the third  fallback",
             RunMode = RunMode.Throughput, NumberOfIterations = 13, RunTimeMilliseconds = 1000,
             TestMode = TestMode.Measurement)]
-        [CounterMeasurement(CreateFallbackCounterName)]
+        [CounterMeasurement(LookupFallbackCounterName)]
         [GcMeasurement(GcMetric.TotalCollections, GcGeneration.AllGc)]
         [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
         public void Lookup_Config_Fallback_3_Deep(BenchmarkContext context)
         {
             L2.GetString("akka.remote.dot-netty.tcp.public-hostname");
-            _createFallbackCounter.Increment();
+            _lookupFallbackOp.Increment();
         }
     }
 }
